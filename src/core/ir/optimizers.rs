8ebca5b4ebf06fbc8f81dbc687cfc9c5b826a294
sirh3e <sirh3e@protonmail.com>
2023-02-03 20:11:42 +0100

    refactored optimize_stage_02

--- src/core/ir/optimizers.rs
+++ src/core/ir/optimizers.rs
@@ -37,8 +37,9 @@ impl ConcatOptimizer {
         }
         optimized
     }
+
     fn optimize_stage_02(expressions: &[Expression]) -> Vec<Expression> {
-        let mut optimized = vec![];
+        let mut optimized: Vec<Expression> = vec![];
         //ToDo optimize [IncVal(5), DecVal(6)] -> [DecVal(1)]
         //ToDo optimize [IncVal(5), DecVal(4)] -> [IncVal(1)]
         //ToDo optimize [IncVal(5), DecVal(5)] -> ()
@@ -47,43 +48,66 @@ impl ConcatOptimizer {
         //ToDo optimize [IncPtr(5), DecPtr(5)] -> ()
         //ToDo optimize []                     -> ()
 
-        for expr in expressions.chunks(2) {
-            match expr {
-                [Expression::DecVal(lhs), Expression::DecVal(rhs)] => {
-                    optimized.push(Expression::DecVal(lhs + rhs))
+        let mut optimized = vec![];
+        for expression in expressions {
+            match (expression, optimized.last()) {
+                (Expression::IncVal(val), Some(&Expression::IncVal(amount))) => {
+                    replace_last(&mut optimized, Expression::IncVal(amount + val))
                 }
-                [Expression::DecVal(lhs), Expression::IncVal(rhs)] => {
-                    if lhs < rhs {
-                        optimized.push(Expression::IncVal(rhs - lhs))
-                    } else if lhs > rhs {
-                        optimized.push(Expression::DecVal(lhs - rhs))
+                (Expression::IncVal(val), Some(&Expression::DecVal(amount))) => {
+                    if val < &amount {
+                        let expression = Expression::DecVal(amount - val);
+                        replace_last(&mut optimized, expression);
+                    } else if &amount < val {
+                        let expression = Expression::IncVal(val - amount);
+                        replace_last(&mut optimized, expression);
                     }
                 }
-                [Expression::IncVal(lhs), Expression::DecVal(rhs)] => {
-                    if lhs < rhs {
-                        optimized.push(Expression::DecVal(rhs - lhs))
-                    } else if lhs > rhs {
-                        optimized.push(Expression::IncVal(lhs - rhs))
+                (Expression::DecVal(val), Some(&Expression::DecVal(amount))) => {
+                    replace_last(&mut optimized, Expression::DecVal(amount + val))
+                }
+                (Expression::DecVal(val), Some(&Expression::IncVal(amount))) => {
+                    if val < &amount {
+                        let expression = Expression::IncVal(amount - val);
+                        replace_last(&mut optimized, expression);
+                    } else if &amount < val {
+                        let expression = Expression::DecVal(val - amount);
+                        replace_last(&mut optimized, expression);
                     }
                 }
-                [Expression::IncVal(lhs), Expression::IncVal(rhs)] => {
-                    optimized.push(Expression::IncVal(lhs + rhs))
+                (Expression::IncPtr(val), Some(&Expression::IncPtr(amount))) => {
+                    replace_last(&mut optimized, Expression::IncPtr(amount + val))
                 }
-                [Expression::Loop(lhs_expressions), Expression::Loop(rhs_expressions)] => {
-                    optimized.push(Expression::Loop(Self::optimize_stage_02(lhs_expressions)));
-                    optimized.push(Expression::Loop(Self::optimize_stage_02(rhs_expressions)));
+                (Expression::IncPtr(val), Some(&Expression::DecPtr(amount))) => {
+                    if val < &amount {
+                        let expression = Expression::DecPtr(amount - val);
+                        replace_last(&mut optimized, expression);
+                    } else if &amount < val {
+                        let expression = Expression::IncPtr(val - amount);
+                        replace_last(&mut optimized, expression);
+                    }
                 }
-                [Expression::Loop(expressions), expression] => {
-                    optimized.push(Expression::Loop(Self::optimize_stage_02(expressions)));
-                    optimized.push(expression.clone()); //ToDo not sure loop inc 1 dec 1 => should be 0
+                (Expression::DecPtr(val), Some(&Expression::DecPtr(amount))) => {
+                    replace_last(&mut optimized, Expression::DecPtr(amount + val))
                 }
-                [Expression::Loop(expressions)] => {
-                    optimized.push(Expression::Loop(Self::optimize_stage_02(expressions)))
+                (Expression::DecPtr(val), Some(&Expression::IncPtr(amount))) => {
+                    if val < &amount {
+                        let expression = Expression::IncPtr(amount - val);
+                        replace_last(&mut optimized, expression);
+                    } else if &amount < val {
+                        let expression = Expression::DecPtr(val - amount);
+                        replace_last(&mut optimized, expression);
+                    }
                 }
-                _ => optimized.extend(Vec::from(expressions)),
+                (Expression::Loop(expressions), _) => {
+                    let sub_expressions = Self::optimize_stage_02(&expressions);
+                    if sub_expressions.len() > 0 {
+                        optimized.push(Expression::Loop(Self::optimize_stage_02(&sub_expressions)))
+                    }
+                }
+                (expression, _) => optimized.push(expression.clone()),
             }
         }
-
         optimized
     }
 }
